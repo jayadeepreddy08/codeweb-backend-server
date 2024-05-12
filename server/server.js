2a63dca5693d65a8f240c77a543d5aef2f798897
Jayadeep <jayadeepreddy2002@gmail.com>
2024-05-12 14:46:23 +0530

    cors 1

--- server/server.js
+++ server/server.js
@@ -4,6 +4,13 @@ const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors'); 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(cors({
     origin: 'https://codeweb-front-client.onrender.com/'// Allow requests only from this origin
     
